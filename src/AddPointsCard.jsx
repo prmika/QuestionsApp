51096c1256790fe657ab0d4ca49613fac4f61e05
Miika Kauppinen <90893994+prmika@users.noreply.github.com>
2024-08-01 21:16:01 +0300

    Focus on input when component is opened and style change

--- src/AddPointsCard.jsx
+++ src/AddPointsCard.jsx
@@ -1,22 +1,36 @@
-  import React, { useEffect, useState } from "react";
-  import { useTranslation } from "react-i18next";
-  
-  function AddPointsCard(props) {
-    const [points, setPoints] = useState(0);
-    const team = props.team;
-    const { t } = useTranslation();
-  
-    const handleAddPoints = () => {
-      props.onAddPoints(points, null, team.id);
-    };
-  
-    return (
+import React, { useEffect, useRef, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+function AddPointsCard(props) {
+  const [points, setPoints] = useState(0);
+  const team = props.team;
+  const { t } = useTranslation();
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
+  const handleAddPoints = () => {
+    props.onAddPoints(points, null, team.id);
+  };
+
+  return (
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        handleAddPoints();
+      }}
+    >
       <div className="cardOverlay">
         <h1>
           {t("add_points_to_team")}: {team.name}
         </h1>
         <div style={{ marginTop: "120px" }}>
           <input
+            ref={inputRef}
             className="input"
             type="number"
             onChange={(e) => setPoints(e.target.value)}
@@ -26,7 +40,8 @@
           </button>
         </div>
       </div>
-    );
-  }
-  
-  export default AddPointsCard;
+    </form>
+  );
+}
+
+export default AddPointsCard;
